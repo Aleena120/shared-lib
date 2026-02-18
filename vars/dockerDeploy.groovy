213dd9c52b6119416061f6a122484e43afaf4e0a
Aleena Johnson <117493734+Aleena120@users.noreply.github.com>
2026-02-18 23:26:49 +0530

    Fix port mapping in Docker deployment script

--- vars/dockerDeploy.groovy
+++ vars/dockerDeploy.groovy
@@ -1,6 +1,9 @@
 def call(Map config) {
-    echo "Deploying Docker container ${config.containerName} from image ${config.imageName} on port ${config.portMapping}"
+
+    echo "Deploying Docker container ${config.containerName} from image ${config.imageName} on port ${config.hostPort}"
+
     sh "docker stop ${config.containerName} || true"
     sh "docker rm ${config.containerName} || true"
-    sh "docker run -d --name ${config.containerName} -p ${config.portMapping}:3000 ${config.imageName}"
+
+    sh "docker run -d --name ${config.containerName} -p ${config.hostPort}:3000 ${config.imageName}"
 }
